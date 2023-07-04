8d6d92c9501f968e44cc1a64e0592ce07296188f
svenkatesh0090 <venkateshcsp9@gmail.com>
2023-07-04 11:49:22 +0530

    Done

--- src/main/java/com/driver/repository/WebSeriesRepository.java
+++ src/main/java/com/driver/repository/WebSeriesRepository.java
@@ -2,8 +2,11 @@ package com.driver.repository;
 
 import com.driver.model.WebSeries;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface WebSeriesRepository extends JpaRepository<WebSeries,Integer> {
 
-    WebSeries findBySeriesName(String seriesName);
+    Optional<WebSeries> findBySeriesName(String seriesName);
 }
